d56ff5f32071671682e49728d4450da073c82229
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-07-28 13:56:35 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,62 @@
 import React,{Component} from 'react';
+import { nanoid } from 'nanoid'
 // import PropTypes from 'prop-types'
 import ToDoList from './toDoList'
+import ToDoEditor from './toDoEditor/index';
+import Filter from './toDoEditor/filter';
+const uId = nanoid(5)
+
 class App extends Component{
   state={
     todos:[{id:'id-1',text:'Вивчити основи React',completed:false},
     {id:'id-2',text:'Розібратися с React Router',completed:false},
-    {id:'id-3',text:'Пережити Redux',completed:false},]
+    {id:'id-3',text:'Пережити Redux',completed:false},],
+    filter:''
+  };
+
+addToDo=text=>{
+
+  const todo={
+    id:uId,
+    text,
+    completed:false,
+
   }
+  this.setState(({todos})=>({todos:[...todos,todo]}))
+}
+
+changeFilter=(e)=>{
+  this.setState({filter:e.currentTarget.value})
   
-  
+}
+   deleteToDo=id=>{
+    this.setState(prevState=>({todos:prevState.todos.filter(todo=>todo.id!==id)}))
+  };
+  toggleCompleted = toDoId=>{
+    
+    this.setState(({todos}) =>({todos:todos.map(todo=>
+     todo.id===toDoId?{...todo,completed:!todo.completed}:todo
+  ) 
+
+    }))
+  }
+  visibleToDo=()=>{
+    const {todos,filter} = this.state 
+    const normalize = this.state.filter.toLocaleLowerCase()
+    return todos.filter(todo=>todo.text.toLocaleLowerCase().includes(normalize))
+
+  }
   render(){
-   const {todos} = this.state 
+   const {todos,filter} = this.state 
+   const filterToDo = this.visibleToDo()
+     
     return(
 <>
 <h1>hello</h1>
-<ToDoList todos={todos}/>
+<ToDoEditor onSubmit={this.addToDo}/>
+<Filter value={filter} onChange={this.changeFilter}/>
+
+<ToDoList todos={filterToDo} onDeleteToDo={this.deleteToDo} onToggleComleted={this.toggleCompleted}/>
 
 
 </>
--- src/components/toDoList/index.jsx
+++ src/components/toDoList/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-const ToDoList =({todos})=>{
-return <ul>{todos.map(({id,text})=><li key={id}><p>{text}</p></li>)}</ul>
+import {useStyles}from './styles'
+const ToDoList =({todos,onDeleteToDo,onToggleComleted})=>{
+const styles = useStyles()
+
+    return <ul className={styles.todolist}>
+        {todos.map(({id,text,completed})=><li key={id} className={styles.todoList__items}>
+            <input type="checkbox" checked={completed} onChange={()=>onToggleComleted(id)}></input>
+            <p className={styles.items__text}>{text}</p>
+            <button onClick={()=>onDeleteToDo(id)}>delate</button>
+            </li>)}
+            </ul>
 }
 
 
